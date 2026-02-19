6867894d06f2b8a08ba478ba02965aa428d9a5b6
Octavio Roque <or94502@gmail.com>
2026-02-19 16:37:53 -0600

    first commit

---
+++ AF1_Equipo6/agenda21-web/script.js
@@ -0,0 +1,4 @@
+function mostrarDato() {
+    document.getElementById("datoCurioso").innerHTML =
+    "La Agenda 21 contiene más de 40 capítulos que abordan desde la pobreza hasta la gestión de recursos naturales.";
+}
